39b618281625b1942b27dd59d6a44fcd1a95ab7b
akshayakotra <akshaya6705@gmail.com>
2026-04-09 01:41:16 +0530

    Initialize Flask app with addition functionality

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask ( __name__ )
+def add (a , b ) :
+  return a + b
+@app . route ( " / " )
+def home () :
+  return " Addition : ␣ " + str ( add (5 , 3) )
+if __name__ == " __main__ " :
+  app . run ( host = " 0.0.0.0 " , port =5000)
